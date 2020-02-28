b0c1dc6b5a72610cb4e4a5b0e512d91557179102
João Vitor <joaovitorcoelho13@gmail.com>
2020-02-28 15:25:34 -0300

    add page home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, ProductList, Product, ProductImage, ProductTitle, ProductPrice, ButtonText, Amount, AddButton, ProductAmount } from './styles';
+
 
 export default function Home() {
   return (
     <Container>
-      <Text>Home</Text>
+      <ProductList>
+        <Product>
+          <ProductImage source={{ uri: "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" }} />
+          <ProductTitle>Tênis de Caminhada Leve Confortável</ProductTitle>
+          <ProductPrice>R$176,90</ProductPrice>
+          <AddButton>
+            <ProductAmount>
+              <Icon name="add-shopping-cart" color="#FFF" size={20} />
+              <Amount>0</Amount>
+            </ProductAmount>
+            <ButtonText>ADICIONAR</ButtonText>
+          </AddButton>
+        </Product>
+      </ProductList>
     </Container>
 
   )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,65 @@
 import styled from 'styled-components/native';
+import { darken } from 'polished';
 
 export const Container = styled.View`
   flex: 1;
   background: #191920;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 20px;
+`;
+
+export const ProductList = styled.View``;
+
+export const Product = styled.View`
+  background: #fff;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 4px;
+  width: 270px;
+`;
+
+export const ProductImage = styled.Image`
+  height: 250px;
+  width: 250px;
+`;
+
+export const ProductTitle = styled.Text`
+ font-size: 16px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 28px;
+  font-weight: bold;
+`;
+
+export const AddButton = styled.TouchableOpacity`
+ background: #7159c1;
+ flex-direction: row;
+ align-items: center;
+ border-radius: 4px;
+ margin-top: auto;
+`;
+
+export const ProductAmount = styled.View`
+  background: ${darken(0.03, "#7159c1")};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  flex-direction: row;
+  padding: 12px;
+  align-items: center;
+`;
+
+export const Amount = styled.Text`
+  color: #fff;
+  margin: 0px 4px 0px 10px;
+`;
+
+export const ButtonText = styled.Text`
+  flex: 1;
+  text-align: center;
+  font-weight: bold;
+  color: #fff;
 `;
